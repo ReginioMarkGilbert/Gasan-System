691a42a738a8db9ee5be686c02bd81e57224bf8c
EndzyCodes <127296867+ReginioMarkGilbert@users.noreply.github.com>
2025-01-30 09:17:47 +0800

    Mark: Fix dashboard padding

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -23,11 +23,11 @@ function Dashboard({ tab }) {
     const ComponentToRender = componentMap[tab] || Overview; // Fallback to Overview if tab not found
 
     return (
-        <div className="fixed inset-0 flex bg-gray-100">
+        <div className="fixed inset-0 flex bg-gray-100 ">
             <Sidebar />
             <div className="flex-1 flex flex-col min-h-0">
                 <Header />
-                <main className="flex-1 min-h-0">
+                <main className="flex-1 min-h-0 h-screen overflow-y-auto p-8 my-auto bg-gradient-to-b from-gray-100 to-gray-200">
                     <ComponentToRender />
                 </main>
             </div>
